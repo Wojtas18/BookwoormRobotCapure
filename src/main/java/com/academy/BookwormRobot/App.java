a716c8c867c6354d1c45019084dbf3d27ea4c70a
Paulina Husiatynska <paulina.husiatynska@gmail.com>
2016-03-18 12:49:15 +0100

    Dodanie odpowiedniej zaleznosci z log4j do poma + dodanie log4j.properties i proba uzycia

--- src/main/java/com/academy/BookwormRobot/App.java
+++ src/main/java/com/academy/BookwormRobot/App.java
@@ -1,13 +1,21 @@
 package com.academy.BookwormRobot;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private final static Logger logger = Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info("Hello!");
+        logger.warn("Warning!");
+        logger.error("Ojej error");
+        logger.debug("Debugujemy");
+        logger.fatal("Bardzo niedobrze");
     }
 }
